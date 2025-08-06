c5bf3078803abede05e44f378d19ea1519a946d9
LucaBarboza <luca.barboza@gmail.com>
2025-08-06 02:22:51 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+
+paginas = {
+    "Páginas": [ st.Page("paginas/inicial.py", title="Início", icon='🚓', default=True)],
+    
+    "Exemplo": [ st.Page("paginas/pagina1.py", title="Banco de Dados", icon='🚙'), 
+                st.Page("paginas/pagina2.py", title="Exemplo", icon='⚡')]
+    ]
+}
+
+# Usa a estrutura de páginas final (com ou sem Admin)
+pg = st.navigation(paginas)
+pg.run()
+
+# comentario
