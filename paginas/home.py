f781f416530a77282bd735ad24f67c06d3b54326
LucaBarboza <luca.barboza@gmail.com>
2025-08-06 23:54:06 -0300

    logout

--- paginas/home.py
+++ paginas/home.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 if not st.user.is_logged_in:
-  if st.button:
-    st.login()
-  
-else:
-  st.write(f"Olá {getattr(st.user, 'name', 'Usuário')}!")
+    st.write("Faça o login para continuar.")
+    if st.button("Login"):
+        st.login()
 
-if st.button("Sair"):
-    st.session_state['logged_in'] = False # ou authenticator.logout()
-    st.rerun()
+else:
+    st.write(f"Olá, {st.user.name}!")
+    
+    if st.button("Sair"):
+        st.logout()
