ff727523567231d14d5b432d51a7434321f80cd1
LucaBarboza <luca.barboza@gmail.com>
2025-08-06 23:51:00 -0300

    logout

--- paginas/home.py
+++ paginas/home.py
@@ -6,3 +6,7 @@ if not st.user.is_logged_in:
   
 else:
   st.write(f"Olá {getattr(st.user, 'name', 'Usuário')}!")
+
+if st.button("Sair"):
+    st.session_state['logged_in'] = False # ou authenticator.logout()
+    st.rerun()
