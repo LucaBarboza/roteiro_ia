d065affbe95fe721b0b72fe6fdfb6ffc7696d9e6
LucaBarboza <luca.barboza@gmail.com>
2025-08-11 04:00:44 -0300

    .

--- paginas/ver.py
+++ paginas/ver.py
@@ -29,6 +29,7 @@ if 'roteiro_aberto' not in st.session_state:
 if roteiros:
     for i, roteiro in enumerate(roteiros):
         with st.container(border=True):
+            col1, col2, col3 = st.columns([2, 2, 2]) 
             pais = roteiro.get('pais', 'País Desconhecido')
             emojis = roteiro.get('emojis', '')
             st.subheader(f"{pais} {emojis}", )
@@ -44,41 +45,43 @@ if roteiros:
                 st.header(f"📍  {pais} {emojis}")
                 st.markdown(roteiro['texto'])
                 st.divider()
-            if st.button("🗑️ Deletar", key=f"delete_{i}", help="Deletar este roteiro"):
-                doc_ref = db.collection(colecao).document(st.user.email) 
-                doc_ref.update({
-                    'roteiros': firestore.ArrayRemove([roteiro])
-                })
-                st.success(f"Roteiro para {pais} deletado!")
-                if st.session_state.roteiro_aberto == roteiro['pais']:
-                    st.session_state.roteiro_aberto = None
-                st.rerun()
+            with col3:
+                if st.button("🗑️ Deletar", key=f"delete_{i}", help="Deletar este roteiro"):
+                    doc_ref = db.collection(colecao).document(st.user.email) 
+                    doc_ref.update({
+                        'roteiros': firestore.ArrayRemove([roteiro])
+                    })
+                    st.success(f"Roteiro para {pais} deletado!")
+                    if st.session_state.roteiro_aberto == roteiro['pais']:
+                        st.session_state.roteiro_aberto = None
+                    st.rerun()
             if is_open:
-                if st.button("Baixar como PDF", key=f"PDF_{i}"):
-                    buffer = io.BytesIO()
-                    documentos = SimpleDocTemplate(buffer, pagesize=A4, rightMargin=72, leftMargin=72, topMargin=72, bottomMargin=18)
-                    styles = getSampleStyleSheet()
-                    titulo_style = ParagraphStyle(
-                        'CustomTitle',
-                        parent=styles['Heading1'],
-                        fontSize=24,
-                        spaceAfter=30,
-                        textColor=colors.HexColor('#2E7D32'),
-                        alignment=1
-                        )
-                    subtitulo_style = ParagraphStyle(
-                        'CustomSubtitle',
-                        parent=styles['Heading2'],
-                        fontSize=14,
-                        spaceAfter=12,
-                        textColor=colors.HexColor('#1976D2')
-                        )
+                with col1:
+                    if st.button("Baixar como PDF", key=f"PDF_{i}"):
+                        buffer = io.BytesIO()
+                        documentos = SimpleDocTemplate(buffer, pagesize=A4, rightMargin=72, leftMargin=72, topMargin=72, bottomMargin=18)
+                        styles = getSampleStyleSheet()
+                        titulo_style = ParagraphStyle(
+                            'CustomTitle',
+                            parent=styles['Heading1'],
+                            fontSize=24,
+                            spaceAfter=30,
+                            textColor=colors.HexColor('#2E7D32'),
+                            alignment=1
+                            )
+                        subtitulo_style = ParagraphStyle(
+                            'CustomSubtitle',
+                            parent=styles['Heading2'],
+                            fontSize=14,
+                            spaceAfter=12,
+                            textColor=colors.HexColor('#1976D2')
+                            )
+                            
+                        story = []
                         
-                    story = []
-                    
-                    story.append(Paragraph(f"🗺 Roteiro para {pais} {emojis}", titulo_style))
-                    story.append(Spacer(1, 20))
-                    story.append(roteiro['texto'], subtitulo_style)
+                        story.append(Paragraph(f"🗺 Roteiro para {pais} {emojis}", titulo_style))
+                        story.append(Spacer(1, 20))
+                        story.append(roteiro['texto'], subtitulo_style)
                 
 else:
     st.info("Nenhum roteiro ainda")
