10680fd561bfb7e4419887d1c13d183b732eeda3
LucaBarboza <luca.barboza@gmail.com>
2025-08-11 02:57:10 -0300

    x

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ pg.run()
 
 if st.user.is_logged_in:
     with st.sidebar:
-        if st.button("Logout",
-        key="logout_button_global", 
-        type='secondary',
-        icon=':material/logout:',
-        use_container_width=True):
-         registrar_acao_usuario("Logout", "Usuário fez logout do sistema (botão global)")
-         st.logout()
+        if st.button("Logout", key="logout_button_global", type='secondary', icon=':material/logout:', use_container_width=True):
+            registrar_acao_usuario("Logout", "Usuário fez logout do sistema (botão global)")
+            st.logout()
