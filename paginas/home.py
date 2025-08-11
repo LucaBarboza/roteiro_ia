de24c3f7eae3cb5bf50ec10ff74ef5024b668760
LucaBarboza <luca.barboza@gmail.com>
2025-08-11 02:56:11 -0300

    \

--- paginas/home.py
+++ paginas/home.py
@@ -2,13 +2,12 @@ import streamlit as st
 
 if not st.user.is_logged_in:
     col1, col2, col3 = st.columns([1, 2, 1])
-        with col2:
-        # Logo centralizada
-            st.image('arquivos/teste1.png', width=200, use_container_width=True)
-            st.title("GuIA de Viagem") 
-            st.markdown("Faça login com sua conta Google para começar.")
-            if st.button("Login com Google", type="primary", use_container_width=True, icon=':material/login:'):
-                st.login()
+    with col2:
+        st.image('arquivos/teste1.png', width=200, use_container_width=True)
+        st.title("GuIA de Viagem") 
+        st.markdown("Faça login com sua conta Google para começar.")
+        if st.button("Login com Google", type="primary", use_container_width=True, icon=':material/login:'):
+            st.login()
 
 else:
     st.write(f"Olá, {st.user.name}!")
