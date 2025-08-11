f602038d73961ec0f8a9ef125b958841c8387cbc
LucaBarboza <luca.barboza@gmail.com>
2025-08-11 02:54:35 -0300

    z

--- paginas/home.py
+++ paginas/home.py
@@ -1,14 +1,15 @@
 import streamlit as st
 
-st.title("✈️ Gerador de Roteiros de Viagem")
-
 if not st.user.is_logged_in:
-    st.subheader("Faça o login para continuar.")
-    if st.button("Login"):
-        st.login()
+    col1, col2, col3 = st.columns([1, 2, 1])
+        with col2:
+        # Logo centralizada
+            st.image('arquivos/teste1.png', width=200, use_container_width=True)
+            st.title("GuIA de Viagem") 
+            st.markdown("Faça login com sua conta Google para começar.")
+            if st.button("Login com Google", type="primary", use_container_width=True, icon=':material/login:'):
+                st.login()
 
 else:
     st.write(f"Olá, {st.user.name}!")
     
-    if st.button("Sair"):
-        st.logout()
