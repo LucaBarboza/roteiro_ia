cffd59a94cb75c380aa638c5c3ba9a45d1932578
LucaBarboza <luca.barboza@gmail.com>
2025-08-14 15:26:52 -0300

    .

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ pg.run()
 
 if st.user.is_logged_in:
     st.set_page_config(initial_sidebar_state="expanded")
-    st.sidebar.title("Menu de Navegação")
     
     with st.sidebar:
         if st.button("Logout", key="logout_button_global", type='secondary', icon=':material/logout:', use_container_width=True):
